3992d0f0f5f9c88ce927c00acfbcd8dd6e84b3f9
Erlan <nurlanuuluerlannue@gmail.com>
2024-04-04 11:28:44 +0600

    crs

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ require('dotenv').config();
 const AuthController = require('./controllers/authController');
 const path = require('path');
 const authenticated = require('./middleware/authMiddleware');
+const cors = require('cors');
 
 var port = process.env.PORT || '3000';
 
+app.use(cors());
 app.use(express.static('public'));
 app.use('./css', express.static(__dirname + 'public/css'));
 app.use('./js', express.static(__dirname + 'public/js'));
